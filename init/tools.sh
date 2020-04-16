74031675d176499a4d12c78e6e231d1987451fd9
shendezhi02 <shendezhi02@countrygarden.com.cn>
2020-04-16 10:58:39 +0800

    feat(tools):增加部分脚本

--- init/tools.sh
+++ init/tools.sh
@@ -9,15 +9,26 @@ read -p 'enter a number: ' number
 case $number in
             1)
                         echo '开始安装vscode...'
+                        sudo add-apt-repository "deb [arch=`dpkg --print-architecture`] https://packages.microsoft.com/repos/vscode stable main"
+                        sudo curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+                        sudo apt update
+                        sudo apt install code
             ;;
             2)
                         echo '开始安装docker...'
             ;;
             3) 
                         echo '开始安装git...'
+                        sudo apt update
+                        sudo apt install git
             ;;
             4)
                         echo '开始安装maven...'
+                        if [ `which java`x = ""x ]
+                        then
+                                    echo '未安装jdk，请先安装jdk'
+                                    exit
+                        fi
             ;;
             5)
                         echo '开始安装chrome...'
